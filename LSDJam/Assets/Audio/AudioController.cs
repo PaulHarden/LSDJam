3897689a4d86834168f65505012db185f15c43fa
ThePhard <47338095+PaulHarden@users.noreply.github.com>
2023-11-25 22:36:32 -0500

    Refactored AudioController script from Unpaid Intern, removed everything handling asset loading/unloading

--- LSDJam/Assets/Audio/AudioController.cs
+++ LSDJam/Assets/Audio/AudioController.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 public class AudioController : MonoBehaviour
 {
 	public static AudioController Singleton { get; set; }
-	[SerializeField] private AudioSource _effectsSource;
-	[SerializeField] private AudioSource _musicSource;
-	[field: SerializeField, Tooltip("The lowest pitch variation a single sound can be played with.")]
-	public float MinPitch { get; private set; }
-	[field: SerializeField, Tooltip("The highest pitch variation a single sound can be played with.")]
-	public float MaxPitch { get; private set; }
-	private AudioClip _loadedClip;
+	[SerializeField] private AudioSource effectsSource;
+	[SerializeField] private AudioSource musicSource;
+	public float minPitch, maxPitch;
 	public AudioClip levelMusic;
 	private float _masterVolume;
 	private float _musicVolume;
@@ -22,38 +18,22 @@ public class AudioController : MonoBehaviour
 		{
 			if (levelMusic != Singleton.levelMusic)
 			{
-				AdjustVolume();
 				Singleton.levelMusic = levelMusic;
 				Singleton.PlayLevelMusic();
 			}
 			Destroy(gameObject);
 		}
 		else
-		{
-			AdjustVolume();
 			Singleton = this;
-		}
 		DontDestroyOnLoad(this);
 	}
-
-	/*private void OnEnable()
-	{
-		SettingsUI.OnVolumeChange += AdjustVolume;
-		PauseController.OnVolumeChange += AdjustVolume;
-	}*/
-
-	/*private void OnDisable()
-	{
-		SettingsUI.OnVolumeChange -= AdjustVolume;
-		PauseController.OnVolumeChange -= AdjustVolume;
-	}*/
 
 	private void AdjustVolume()
 	{
 		_masterVolume = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
 		_musicVolume = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
 		_effectVolume = PlayerPrefs.GetFloat("EffectVolume", 0.5f);
-		_musicSource.volume = _masterVolume * _musicVolume;
+		musicSource.volume = _masterVolume * _musicVolume;
 	}
 
 	public void Start() => PlayLevelMusic(); // TODO: remove later.
@@ -62,60 +42,24 @@ public class AudioController : MonoBehaviour
 
 	public void PlaySound(AudioClip clip, float vol)
 	{
-		_effectsSource.volume = vol * _masterVolume * _effectVolume;
-		_effectsSource.pitch = 1f;
-		_effectsSource.PlayOneShot(clip);
+		effectsSource.volume = vol * _masterVolume * _effectVolume;
+		effectsSource.pitch = 1f;
+		effectsSource.PlayOneShot(clip);
 	}
 
-	/*public IEnumerator LoadAndPlaySound(AssetReferenceAudioClip unloadedClip, float vol)
-	{
-		_effectsSource.volume = vol * _masterVolume * _effectVolume;
-		_effectsSource.pitch = 1f;
-		string key = unloadedClip.AssetGUID;
-
-		AsyncOperationHandle<AudioClip> loadOperation = Addressables.LoadAssetAsync<AudioClip>(key);
-		yield return loadOperation;
-
-		if (loadOperation.Status == AsyncOperationStatus.Succeeded)
-		{
-			AudioClip clip = loadOperation.Result;
-			_effectsSource.PlayOneShot(clip);
-			// Wait for the sound to finish playing
-			yield return new WaitForSeconds(clip.length);
-			AddressableManager.UnloadAsset(clip);
-		}
-	}*/
-
-	public void StopSound() => _effectsSource.Stop();
-
-	/*private void UnloadSound()
-	{
-		if (_loadedClip)
-			AddressableManager.UnloadAsset(_loadedClip);
-		_loadedClip = null;
-	}*/
+	public void StopSound() => effectsSource.Stop();
 
 	public void PlayRandomSound(AudioClip clip, float vol)
 	{
-		float randomPitch = Random.Range(MinPitch, MaxPitch);
-		_effectsSource.pitch = randomPitch;
-		_effectsSource.volume = vol * _masterVolume * _effectVolume;
-		_effectsSource.PlayOneShot(clip);
+		float randomPitch = Random.Range(minPitch, maxPitch);
+		effectsSource.pitch = randomPitch;
+		effectsSource.volume = vol * _masterVolume * _effectVolume;
+		effectsSource.PlayOneShot(clip);
 	}
-
-	/*public async void LoadAndPlayMusic(AssetReferenceAudioClip unloadedClip)
-	{
-		UnloadSound();
-		string key = unloadedClip.AssetGUID;
-		_loadedClip = await AddressableManager.LoadAsset<AudioClip>(key);
-		_musicSource.clip = _loadedClip;
-		StartCoroutine(FadeIn(2f));
-	}*/
 
 	public void PlayMusic(AudioClip clip)
 	{
-		//UnloadSound();
-		_musicSource.clip = clip;
+		musicSource.clip = clip;
 		StartCoroutine(FadeIn(2f));
 	}
 
@@ -124,31 +68,30 @@ public class AudioController : MonoBehaviour
 	public IEnumerator FadeIn(float duration)
 	{
 		float t = 0;
-		_musicSource.volume = 0f;
-		_musicSource.Play();
+		musicSource.volume = 0f;
+		musicSource.Play();
 		while (t < duration)
 		{
 			t += Time.deltaTime;
-			_musicSource.volume = Mathf.Lerp(0f, _masterVolume * _musicVolume, t / duration);
+			musicSource.volume = Mathf.Lerp(0f, _masterVolume * _musicVolume, t / duration);
 			yield return null;
 		}
-		_musicSource.volume = _masterVolume * _musicVolume;
+		musicSource.volume = _masterVolume * _musicVolume;
 		yield return null;
 	}
 
 	public IEnumerator FadeOut(float duration)
 	{
 		float currentTime = 0;
-		float startVol = _musicSource.volume;
+		float startVol = musicSource.volume;
 		while (currentTime < duration)
 		{
 			currentTime += Time.deltaTime;
-			_musicSource.volume = Mathf.Lerp(startVol, 0f, currentTime / duration);
-			if (_musicSource.volume <= 0)
-				_musicSource.Stop();
+			musicSource.volume = Mathf.Lerp(startVol, 0f, currentTime / duration);
+			if (musicSource.volume <= 0)
+				musicSource.Stop();
 			yield return null;
 		}
-		//UnloadSound();
 		yield return null;
 	}
 }
