cf2c60fde890c6db140657681d170fe02b5c46fb
ThePhard <47338095+PaulHarden@users.noreply.github.com>
2024-01-25 21:07:29 -0500

    Added door locked sound to scene transition door and scripted requiring a key

--- LSDJam/Assets/Player/SceneTransition.cs
+++ LSDJam/Assets/Player/SceneTransition.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Audio;
 using Collectables;
 using UI.HUD;
 using UnityEngine;
@@ -6,20 +7,35 @@ using UnityEngine.SceneManagement;
 
 namespace Player
 {
-    public class SceneTransition : Interactable
+    public class SceneTransition : Chest
     {
-        public ItemData requiredItem;
+        private bool _isLoading;
         
         public override void OnInteract()
         {
             if (requiredItem != null)
+            {
                 for(var i = 0; i < Inventory.inventory.Count; i++)
                     if (Inventory.inventory[i].itemData.id == requiredItem.id)
+                    {
+                        _isLoading = true;
+                        if (!AudioController.Singleton.effectsSource.isPlaying)
+                            AudioController.Singleton.PlaySound(unlockedSound, 1f);
                         StartCoroutine(LoadNextScene());
+                    }
+            }
+
+            if (lockedSound != null && !_isLoading)
+            {
+                if (!AudioController.Singleton.effectsSource.isPlaying)
+                    AudioController.Singleton.PlaySound(lockedSound, 1f);
+                requiredPrompt.enabled = true;                
+            }
         }
 
         private IEnumerator LoadNextScene()
         {
+            requiredPrompt.enabled = false;
             GameObject.Find("HUD").GetComponent<HUDController>().fade.Play("A_FadeOut");
             yield return new WaitForSeconds(3f);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   
