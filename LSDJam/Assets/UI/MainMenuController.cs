2aae8faaf7443e1e3ebf290d83b45e14a6fe879c
ThePhard <47338095+PaulHarden@users.noreply.github.com>
2024-02-10 20:48:04 -0500

    Created scene for main menu with basic layout

---
+++ LSDJam/Assets/UI/MainMenuController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace UI
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        private void StartGame()
+        {
+            
+        }
+
+        private void LaunchWebsite()
+        {
+            
+        }
+
+        private void QuitGame()
+        {
+            
+        }
+    }
+}
