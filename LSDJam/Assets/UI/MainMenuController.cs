62d60b63853127383f6c07bb3c85abc6c630c6bb
ThePhard <47338095+PaulHarden@users.noreply.github.com>
2024-02-11 15:19:31 -0500

    Scripted option to fullscreen game and resize window in real time

--- LSDJam/Assets/UI/MainMenuController.cs
+++ LSDJam/Assets/UI/MainMenuController.cs
@@ -5,15 +5,19 @@ namespace UI
 {
     public class MainMenuController : MonoBehaviour
     {
+        public GameObject optionsMenu;
         public GameObject confirmationPrompt;
 
-        private void Start() => HidePrompt();
-
+        private void Start()
+        {
+            HideOptions();
+            HidePrompt();   
+        }
         public void StartGame() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
         public void LaunchPaulWebsite() => Application.OpenURL("https://paulharden.net/");
         public void LaunchMikeWebsite() => Application.OpenURL("https://www.mfgstudio.art/");
-
+        public void ShowOptions() => optionsMenu.SetActive(true);
+        public void HideOptions() => optionsMenu.SetActive(false);
         public void ShowPrompt() => confirmationPrompt.SetActive(true);
         public void HidePrompt() => confirmationPrompt.SetActive(false);
         public void QuitGame() => Application.Quit();
